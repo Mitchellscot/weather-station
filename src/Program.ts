a7c3878d34f674ef91baf48c47143dab4f1631eb
Mitchell Scott <mitchellscott@me.com>
2022-04-15 15:31:28 -0500

    edited longitude

--- src/Program.ts
+++ src/Program.ts
@@ -4,6 +4,6 @@ import DetermineScheduledHours from "./Modules/Schedule";
 import { TurnOff } from "./Modules/LCD";
 const apiKey = process.env.WEATHER_API_KEY
 const latitude = process.env.LATITUDE
-const longitude = process.env.LATITUDE
+const longitude = process.env.LONGITUDE
 
 DetermineScheduledHours() ? RunProgram(apiKey, latitude, longitude) : TurnOff();
